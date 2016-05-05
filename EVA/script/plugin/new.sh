b8d8ae39570d31245b93cf97e26081f8683d14f8
femrat <femrat@gmail.com>
2016-05-05 14:02:39 +0800

    plugin/new.sh: more check before copy, correct remote filename
    stop operation if the directory already exists, or filename invalid.
    correct filename if it is a remote filename which needs scp
    change usage message

--- EVA/script/plugin/new.sh
+++ EVA/script/plugin/new.sh
@@ -4,19 +4,45 @@
 # param: out para
 
 if [ "$#" -ne 2 ]; then
-	echo "Usage: eva new out para"
+	echo "Usage: eva new <.out> <parallel num>"
 	exit 1
 fi
 
-if [ ! -x $1 -o ! -f $1 ]; then
-	echo "$1 is not an executable file"
-	exit 2
+out="$1"
+para="$2"
+
+if echo "$out" | grep ':' >/dev/null; then
+	# remote file
+	name=$(basename "${out##*:}")
+else
+	# local file
+	if [ ! -x "$out" -o ! -f "$out" ]; then
+		echo "$out not found or is not an executable file"
+		exit 2
+	fi
+	name="$out"
 fi
 
-dir="$(basename $1 .out)-p$2"
+name="$(basename "$name" .out)"
+
+if echo "$name" | grep '[- ]' >/dev/null; then
+	echo "\"$name\" is invalid, either \"-\" or space is not allowed"
+	exit 3
+fi
+
+dir="$name-p$para"
+
+if [ -e "$dir" ]; then
+	echo "$dir exists ... nothing to do"
+	exit 4
+fi
 
 mkdir -v "$dir"
 
-scp $1 $dir
-
-chmod -w $dir/*
+echo "copy $out -> $dir/$out"
+if scp $out $dir; then
+	chmod -w $dir/*
+else
+	echo "something wrong. revert ..."
+	rm -vr "$dir"
+fi
